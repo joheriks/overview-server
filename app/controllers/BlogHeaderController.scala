fe36b239be5447da6554cf7db4352b6f31eaf9ed
Ethan Resnick <ethan.resnick@gmail.com>
2015-06-02 11:58:49 -0400

    Serve blog header

---
+++ app/controllers/BlogHeaderController.scala
@@ -0,0 +1,11 @@
+package controllers
+
+import controllers.auth.OptionallyAuthorizedAction
+import controllers.auth.Authorities.anyUser
+
+object BlogHeaderController extends Controller {
+  def show() = OptionallyAuthorizedAction(anyUser) { implicit request =>
+    Ok(views.html.components.navbar(None, None))
+  }
+}
+
